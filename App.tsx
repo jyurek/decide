99f676184deedbf991c053379e4d2833125874f4
Jon Yurek <jyurek@gmail.com>
2020-08-03 21:17:05 -0400

    Attempting a selection

--- App.tsx
+++ App.tsx
@@ -1,13 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  AppState,
+  ScrollView,
+} from 'react-native';
+
+const ordinalNames = [
+  'first',
+  'second',
+  'third',
+  'fourth',
+  'fifth',
+  'sixth',
+  'seventh',
+  'eighth',
+  'ninth',
+  'tenth',
+];
+
+const decide = (optionCount: number) => Math.floor(Math.random() * optionCount);
 
 export default function App() {
+  const [decision, setDecision] = React.useState<number>(decide(2));
+  const askAgain = React.useCallback(() => {
+    const newDecision = decide(2);
+    setDecision(newDecision);
+  }, []);
+
+  const onAppStateChange = newAppState => {
+    if (newAppState === 'active') {
+      askAgain();
+    }
+  };
+
+  React.useEffect(() => {
+    AppState.addEventListener('change', onAppStateChange);
+    return () => {
+      AppState.removeEventListener('change', onAppStateChange);
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.top} />
+      <TouchableOpacity onPress={askAgain} style={styles.middle}>
+        <Text style={styles.label}>
+          Pick the {ordinalNames[decision]} option.
+        </Text>
+      </TouchableOpacity>
+      <View style={styles.bottom}>
+        <Text>Out of</Text>
+        <ScrollView
+          style={{height: 100, padding: 2}}
+          snapToInterval={16}
+          contentContainerStyle={{
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>2</Text>
+          <Text>3</Text>
+          <Text>4</Text>
+          <Text>5</Text>
+          <Text>6</Text>
+          <Text>7</Text>
+          <Text>8</Text>
+          <Text>9</Text>
+          <Text>10</Text>
+        </ScrollView>
+        <Text>Options</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,5 +85,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  top: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  middle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottom: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 20,
   },
 });
